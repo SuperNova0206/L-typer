159c9a098cce7fb9c5d0c181cb22ee4f91ca3845
SuperNova0206 <konichyoussef@gmail.com>
2026-07-11 02:48:30 +0100

    :) creating one or two commands with their arguments and options

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import typer as tp
+import typer 
 
+app = typer.Typer()
+
+@app.command()
 def main(name="world") -> None : 
 	print(f"Hello {name.capitalize()}!")
 
-if __name__ == "__main__" : tp.run(main)
+@app.command()
+def goodbye(name : str, formal : bool = False) -> None : 
+	if formal: 
+		print(f"Goodbye Ms. {name.capitalize()}. have a good day.")
+	else :
+		print(f"Bye {name.capitalize()}!")
+
+if __name__ == "__main__" : app()
