c31a7b36e97e2c59698bc7e4e9333e28a75936fc
SuperNova0206 <konichyoussef@gmail.com>
2026-07-12 21:16:40 +0100

    :( Error Line: 28 I can't create a costumized type hint

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import typer 
+from rich import print
+from typing import TypedDict
+
+class UserData(TypedDict) : 
+	name : str
+	age : int
+	items :  list[dict[str, str], dict[str, str]]
+	active : bool
+	affiliation : None
+
+data : UserData = {
+	"name" : "Rick",
+	"age" : 42,
+	"items" : [{"name": "Portal Gun"}, {"name": "Plumbus"}],
+	"active" : True,
+	"affiliation" : None
+}
 
 app = typer.Typer()
 
 @app.command()
-def main(name="world") -> None : 
+def hello(name : str = "world" ) -> None : 
 	print(f"Hello {name.capitalize()}!")
+
+# I got an error I can't use a costumized type hint.
+@app.command()
+def info(data : UserData = data) -> None : 
+	print("User data: ")
+	print(data)
 
 @app.command()
 def goodbye(name : str, formal : bool = False) -> None : 
